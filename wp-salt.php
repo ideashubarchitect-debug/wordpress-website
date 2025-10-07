a55874bd4962ac2fed6da2d7bf0b72ac9b1115bb
barcodemine-creator <barcodemine@gmail.com>
2025-10-08 04:50:46 +0530

    Initial commit: WordPress installation with custom .gitignore

---
+++ wp-salt.php
@@ -0,0 +1,10 @@
+<?php
+
+define('AUTH_KEY',         'atoDyAcY9pDJUf5hnthmJeAEtqh7tLX32M6ASNwMuMpUnjan1KTK7aLQRtbBAqFG');
+define('SECURE_AUTH_KEY',  'fh5NqMChiDD0jSQf61UMFmQTB6Lwmp7nbjPzaG67PC8Wmx8A0x3VHmxTqW6QHu84');
+define('LOGGED_IN_KEY',    'ouctqU8ERJoVC0o3CrzNdYA3KnjL6m7oDjWjLtz1CL5Vy4TaAzjNdVXPV94qJyaw');
+define('NONCE_KEY',        'hUoV0GE3Gnd1dQ1xy5sdcIL7K3Fvxa5j8mjS69nCvD8wWmLWbapowzdHq0WaheBT');
+define('AUTH_SALT',        'SfoKY9RGwQRytBYVsvB03rEn0NESz8c0fx9fc7Tt34gdF6N8UEUbGjauT1S68LF6');
+define('SECURE_AUTH_SALT', 'xpLxcuRitPeGsx5M5U7hUu6g8TKaFhtib9864nBTR1C9o6x48o8xvWgLahp7Yn4H');
+define('LOGGED_IN_SALT',   'GGPWN2v4qEb9GwbYHC4bpBqgvKNLDLDSI4qRdmzW6HNNFS3yFAhR7nnmD87gEQhS');
+define('NONCE_SALT',       'w7TnKtVzDrznJqoBgiPz5bocIPGGgu12RVbr8JvQdj10PI0MGcLu0pT374IgWWKR');
